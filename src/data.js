2082a03eeaf2a802e4e0b02bc95556d59f4b35ef
Huy Quoc <proimpossible0809@gmail.com>
2023-03-10 17:09:59 +0700

    update

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://i.imgur.com/V5g2jTx.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "https://i.imgur.com/tMZednG.png",
+      link: "",
+    },
+    {
+      id: 3,
+      img: "https://i.imgur.com/0t8Zjjq.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "https://i.imgur.com/F8zu77S.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "https://i.imgur.com/TnIpsUU.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "https://i.imgur.com/0gBT4HD.png",
+      link: "http://lama.dev",
+    },
+  ];
